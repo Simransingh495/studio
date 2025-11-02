e01e0fbff1e30137e6b861bd6e52808f969b1444
SIMRAN SINGH BTA <bwubta22283@brainwareuniversity.ac.in>
2025-11-02 12:39:17 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6305751481-dce16",
+  "appId": "1:472716528063:web:d230591d02afb5ad9b390a",
+  "apiKey": "AIzaSyCDFGGulsay0dAuWI_OVJAyk9UAN_QZEDI",
+  "authDomain": "studio-6305751481-dce16.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "472716528063"
+};
