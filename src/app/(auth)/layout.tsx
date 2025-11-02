0cdaa956bd8c2b2fdf61f32fd7027433efac495e
SIMRAN SINGH BTA <bwubta22283@brainwareuniversity.ac.in>
2025-11-02 12:49:15 +0000

    Try fixing this error: `Runtime Error: Error: useFirebase must be used w

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,4 +1,6 @@
+
 import { Logo } from '@/components/logo';
+import { FirebaseClientProvider } from '@/firebase/client-provider';
 import Link from 'next/link';
 
 export default function AuthLayout({
@@ -7,13 +9,15 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-secondary/50 p-4">
-       <div className="absolute top-4 left-4">
-        <Link href="/">
-          <Logo />
-        </Link>
+    <FirebaseClientProvider>
+      <div className="flex min-h-screen flex-col items-center justify-center bg-secondary/50 p-4">
+        <div className="absolute top-4 left-4">
+          <Link href="/">
+            <Logo />
+          </Link>
+        </div>
+        {children}
       </div>
-      {children}
-    </div>
+    </FirebaseClientProvider>
   );
 }
