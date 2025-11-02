4db5b73d0708046c99810e62f92eb65c8d3f19f1
SIMRAN SINGH BTA <bwubta22283@brainwareuniversity.ac.in>
2025-11-02 11:14:19 +0000

    please use this as the logo same picture

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -10,33 +10,21 @@ export function Logo({ className }: { className?: string }) {
     >
       <svg
         className="h-8 w-8"
-        viewBox="0 0 100 100"
-        fill="currentColor"
+        viewBox="0 0 64 64"
         xmlns="http://www.w3.org/2000/svg"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        stroke-linejoin="round"
+        stroke-miterlimit="2"
       >
         <path
-          fillRule="evenodd"
-          clipRule="evenodd"
-          d="M50 0C50 0 70 18 70 35C70 52 50 70 50 70C50 70 30 52 30 35C30 18 50 0 50 0ZM50 10C61.0457 10 70 18.9543 70 30C70 41.0457 61.0457 50 50 50C38.9543 50 30 41.0457 30 30C30 18.9543 38.9543 10 50 10Z"
-          transform="translate(0, 5)"
-        />
-        <path d="M25 50C15 65 15 90 15 90L35 90L37 80L40 90L43 75L47 90L50 60" stroke="currentColor" strokeWidth="5" strokeLinecap="round" strokeLinejoin="round" fill="none"/>
-        <path d="M75 50C85 65 85 90 85 90L65 90L63 80L60 90L57 75L53 90L50 60" stroke="currentColor" strokeWidth="5" strokeLinecap="round" strokeLinejoin="round" fill="none"/>
-        <path
-          d="M20,60 C30,40 40,70 50,60"
-          stroke="currentColor"
-          strokeWidth="4"
-          fill="none"
-          strokeLinecap="round"
-          transform="translate(-1, 28) scale(1.1)"
+          d="M32.012 5.343c-6.895 8.95-16.32 20.72-16.32 30.655 0 8.998 7.302 16.32 16.32 16.32 9.018 0 16.32-7.322 16.32-16.32C48.332 26.062 38.907 14.293 32.012 5.343m3.881 33.213c-2.316 2.317-5.518 4.218-8.156 4.312-3.411.12-6.527-1.533-8.832-4.303-4.28-5.143 1.258-12.01 1.258-12.01s2.08-1.897 4.654-2.583c2.783-.75 5.257.02 5.257.02.01.002 4.14 1.763 6.303 4.966 2.454 3.633 1.83 9.478-1.484 12.443"
+          fill="currentColor"
+          fill-rule="nonzero"
         />
         <path
-          d="M80,60 C70,40 60,70 50,60"
-          stroke="currentColor"
-          strokeWidth="4"
-          fill="none"
-          strokeLinecap="round"
-          transform="translate(1, 28) scale(1.1)"
+          d="M36.19 38.21c-2.316 2.317-5.518 4.218-8.156 4.312-3.411.12-6.527-1.533-8.832-4.303-4.28-5.143 1.258-12.01 1.258-12.01s2.08-1.897 4.654-2.583c2.783-.75 5.257.02 5.257.02.01.002 4.14 1.763 6.303 4.966 2.454 3.633 1.83 9.478-1.484 12.443z"
+          fill="#fff"
         />
       </svg>
       <span className="font-headline">BloodSync</span>
