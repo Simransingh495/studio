c03be825206ec1160fafe738b7ae5297a7debdd9
SIMRAN SINGH BTA <bwubta22283@brainwareuniversity.ac.in>
2025-11-03 04:38:06 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased" suppressHydrationWarning>
+      <body className="font-body antialiased">
         <FirebaseClientProvider>
           {children}
         </FirebaseClientProvider>
